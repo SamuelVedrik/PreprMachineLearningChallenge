99b98919c70fe9b2aa9f84b06fe843500f554d11
SamuelVedrik <38094177+SamuelVedrik@users.noreply.github.com>
2020-04-03 12:56:17 -0400

    Create NLTKsetup.py

---
+++ NLTKsetup.py
@@ -0,0 +1,8 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+import nltk
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+nltk.download('stopwords')
